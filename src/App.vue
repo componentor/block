364f29d6a7b87c974d5667981ab66716937aba96
Steffan <steffanhalv@gmail.com>
2025-12-13 14:44:50 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<component :is="tag">
+		<slot>
+			Drop anything here!
+		</slot>
+	</component>
+</template>
+
+<script>
+export default {
+	wysiwyg: true,
+	inject: ['theme', 'breakpoint'],
+	props: {
+		tag: {
+			type: String,
+			default: 'div'
+		}
+	}
+};
+</script>
+
+<style scoped>
+</style>
